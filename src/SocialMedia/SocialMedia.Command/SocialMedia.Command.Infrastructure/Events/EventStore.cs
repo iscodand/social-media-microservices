1087ca8df0bc5fba97ddf27c4fffc744480e24a7
iscodand <iscodand@outlook.com>
2023-08-07 17:54:27 -0300

    add: implementing EventStore :D

--- src/SocialMedia/SocialMedia.Command/SocialMedia.Command.Infrastructure/Events/EventStore.cs
+++ src/SocialMedia/SocialMedia.Command/SocialMedia.Command.Infrastructure/Events/EventStore.cs
@@ -1,6 +1,8 @@
 using CQRS.Core.Domain;
 using CQRS.Core.Events;
+using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
+using SocialMedia.Command.Domain.Aggregates;
 
 namespace SocialMedia.Command.Infrastructure.Events
 {
@@ -13,14 +15,51 @@ namespace SocialMedia.Command.Infrastructure.Events
             _eventStoreRepository = eventStoreRepository;
         }
 
-        public Task<List<BaseEvent>> GetEventsAsync(string aggregateId)
+        public async Task<List<BaseEvent>> GetEventsAsync(string aggregateId)
         {
-            throw new NotImplementedException();
+            List<EventModel> eventStream = await _eventStoreRepository.GetAggregateByIdAsync(aggregateId);
+
+            if (eventStream == null || !eventStream.Any())
+            {
+                throw new AggregateNotFoundException("This aggregate don't exists in the current context");
+            }
+
+            List<BaseEvent> eventData = eventStream.OrderBy(x => x.Version)
+                                                   .Select(x => x.EventData)
+                                                   .ToList();
+
+            return eventData;
         }
 
-        public Task SaveEventsAsync(string aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
+        public async Task SaveEventsAsync(string aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
         {
-            throw new NotImplementedException();
+            List<EventModel> eventStream = await _eventStoreRepository.GetAggregateByIdAsync(aggregateId);
+
+            if (expectedVersion != -1 && eventStream[^1].Version != expectedVersion)
+            {
+                throw new ConcurrencyException();
+            }
+
+            int version = expectedVersion;
+
+            foreach (BaseEvent @event in events)
+            {
+                version++;
+                @event.Version = version;
+                string eventType = @event.GetType().Name;
+
+                EventModel eventModel = new()
+                {
+                    TimeStamp = DateTime.Now,
+                    AggregateIdentifier = aggregateId,
+                    AggregateType = nameof(PostAggregate),
+                    Version = version,
+                    EventType = eventType,
+                    EventData = @event
+                };
+
+                await _eventStoreRepository.SaveAsync(eventModel);
+            }
         }
     }
 }
