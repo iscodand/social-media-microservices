e147af7f8fda5859ac43cbecfd19da5f56ef2a44
iscodand <iscodand@outlook.com>
2023-08-27 12:28:29 -0300

    rename: social media commands

--- src/SocialMedia/SocialMedia.Command/SocialMedia.Command.WebApi/Commands/Post/UpdatePostCommand.cs
+++ src/SocialMedia/SocialMedia.Command/SocialMedia.Command.WebApi/Commands/Post/UpdatePostCommand.cs
@@ -2,7 +2,7 @@ using CQRS.Core.Commands;
 
 namespace SocialMedia.Command.WebApi.Commands
 {
-    public class EditPostCommand : BaseCommand
+    public class UpdatePostCommand : BaseCommand
     {
         public string Message { get; set; }
     }
