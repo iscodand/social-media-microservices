b8e65cdff054f614aebb08e3ccc1fb9930f0a2d4
iscodand <iscodand@outlook.com>
2023-08-27 12:30:00 -0300

    infra: setting up dependency injection projects

--- src/SocialMedia/SocialMedia.Command/SocialMedia.Command.WebApi/Program.cs
+++ src/SocialMedia/SocialMedia.Command/SocialMedia.Command.WebApi/Program.cs
@@ -1,14 +1,42 @@
+using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using SocialMedia.Command.Domain.Aggregates;
+using SocialMedia.Command.Infrastructure.Handlers;
 using SocialMedia.Command.Infrastructure.Config;
+using SocialMedia.Command.Infrastructure.Events;
+using SocialMedia.Command.Infrastructure.Repositories;
+using SocialMedia.Command.WebApi.Commands;
+using Confluent.Kafka;
+using SocialMedia.Command.Infrastructure.Dispatchers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Configuration of MongoDb
+// MongoDB Configuration
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 
+// Kafka Configuration
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
+
+// Dependency Injection
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register command handlers
+ICommandHandler commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+CommandDispatcher dispatcher = new();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<UpdatePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<UpdateCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
