e57b58668fddd1458f64c7a41884916810b002e7
iscodand <iscodand@outlook.com>
2023-08-28 17:00:11 -0300

    fix: remove constructor from base entity

--- src/SocialMedia/SocialMedia.Query/SocialMedia.Query.Domain/Entities/Common/BaseEntity.cs
+++ src/SocialMedia/SocialMedia.Query/SocialMedia.Query.Domain/Entities/Common/BaseEntity.cs
@@ -8,12 +8,5 @@ namespace SocialMedia.Query.Domain.Entities
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-
-        public BaseEntity()
-        {
-            Id = Guid.NewGuid();
-            CreatedAt = DateTime.Now;
-            UpdatedAt = DateTime.Now;
-        }
     }
 }
